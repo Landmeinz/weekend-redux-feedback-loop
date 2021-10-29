f65129abd8012083dd602301c4daa09800719bc3
Eric Meinzer <emeinzer7@gmail.com>
2021-10-28 19:54:37 -0500

    imported redux and react-readux elements needed for project

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+
+// <Provider>
+// </Provider>
 
 ReactDOM.render(
     <App />, 
 document.getElementById('root'));
 registerServiceWorker();
+
+
