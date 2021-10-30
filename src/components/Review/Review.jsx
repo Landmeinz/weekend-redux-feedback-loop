a30bda089bdf1c85a8cb897edb51bd5f5d3ee78a
Eric Meinzer <emeinzer7@gmail.com>
2021-10-30 15:38:30 -0500

    success conditional rendering message after user submits data to db

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -4,6 +4,7 @@ import './Review.css';
 import axios from 'axios';
 import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 function Review() {
 
@@ -15,30 +16,51 @@ function Review() {
         console.log('CLICK submit in the review page');
         axios({
             method: 'POST',
-            url:    '/feedback',
-            data:   feedbackData
+            url: '/feedback',
+            data: feedbackData
         })
-        .then(response => {
-            console.log('POST /feedback response', response);
-        })
-        .catch(error => {
-            console.log('POST /feedback ERROR', error);
-        })
-        dispatch({type: 'CLEAR_ALL'});
+            .then(response => {
+                console.log('POST /feedback response', response);
+            })
+            .catch(error => {
+                console.log('POST /feedback ERROR', error);
+            })
+
+        dispatch({ type: 'CLEAR_ALL' });
+        
+        setSubmitState(true);
     }
 
 
 
-    console.log('this is the feedbackData:', feedbackData);
+    const [submitState, setSubmitState] = useState(false)
 
-    return(
+    
+    const displaySuccess = (
+        <div>
+            <h3>FEEDBACK POST SUCCESS!</h3>
+            <button>START NEW FEEDBACK FORM</button>
+        </div>
+    )
+
+
+    const displayReview = (
         <div>
             <h3>Review Your Feedback</h3>
             <p>FEELING: {feedbackData.feeling}</p>
             <p>UNDERSTANDING: {feedbackData.understanding}</p>
             <p>SUPPORT: {feedbackData.support}</p>
             <p>COMMENTS: {feedbackData.comments}</p>
-            <button onClick={handleSubmitFeedback}>SUBMIT</button>
+        <button onClick={handleSubmitFeedback}>SUBMIT</button>
+    </div>
+    )
+    
+
+    console.log('this is the feedbackData:', feedbackData);
+
+    return (
+        <div>
+            {submitState ? displaySuccess : displayReview}
         </div>
     )
 };
